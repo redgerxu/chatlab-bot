5b9a68ccf3eacefd90c454a85c9c588408429923
narutopig <redgerxu@gmail.com>
2020-11-01 11:47:46 -0800

    kick + ban command

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,20 @@ client.registerCommand("ban", (message: Eris.Message, args: string[]): void => {
     let reason: string | undefined = args.join(" ");
     if (!reason.replace(/\s/g, '').length) reason = undefined;
     try {
-        client.banGuildMember(message.guildID!, message.author.id, 0, reason);
+        client.banGuildMember(message.guildID!, target.id, 0, reason);
+        message.channel.createMessage(`Banned ${target.username}`);
+    } catch {
+        message.channel.createMessage("An error occured. :(")
+    }
+}, {})
+
+client.registerCommand("kick", (message: Eris.Message, args: string[]): void => {
+    const target = message.mentions[0];
+    let reason: string | undefined = args.join(" ");
+    if (!reason.replace(/\s/g, '').length) reason = undefined;
+    try {
+        client.kickGuildMember(message.guildID!, target.id, reason);
+        message.channel.createMessage(`Kicked ${target.username}`);
     } catch {
         message.channel.createMessage("An error occured. :(")
     }
