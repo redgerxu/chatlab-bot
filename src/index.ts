308dae5494636908809b794d632a5c73a344261d
narutopig <redgerxu@gmail.com>
2020-11-02 17:54:42 -0800

    added mute command + removed unecessary args for callback in ban/kick

--- src/index.ts
+++ src/index.ts
@@ -28,15 +28,34 @@ client.registerCommand("kick", (message: Eris.Message, args: string[]): void => 
     let reason: string | undefined = args.join(" ");
     if (!reason.replace(/\s/g, '').length) reason = undefined;
     client.kickGuildMember(message.guildID!, target.id, reason)
-        .then(result => message.channel.createMessage(`Kicked ${target.username}`))
+        .then(() => message.channel.createMessage(`Kicked ${target.username}`))
         .catch((err) => {
             message.channel.createMessage("An error occured. :(")
             console.error(err);
         });
 }, {})
 
+client.registerCommand("mute", (message: Eris.Message, args: string[]): void => {
+    if (!(message.member!.permission.json.manageMessages)) return;
+    const target = message.mentions[0];
+    let reason: string | undefined = args.join(" ");
+    if (!reason.replace(/\s/g, '').length) reason = undefined;
+    const role = message.member!.guild.roles.filter(r => r.name.toLowerCase().includes("mute"))[0];
+    const roles = message.member!.roles;
+    if (roles.includes(role.id)) {
+        message.channel.createMessage(`${target.username} is already muted`);
+        return;
+    }
+    client.addGuildMemberRole(message.guildID!, target.id, role!.id, reason)
+        .then(() => message.channel.createMessage(`Muted ${target.username}`))
+        .catch((err) => {
+            message.channel.createMessage("An error occured. :(");
+            console.error(err);
+        });
+})
+
 client.on("ready", () => {
-    console.log("started");
+    console.log(`${client.user.username} started`);
 })
 
 client.connect();
