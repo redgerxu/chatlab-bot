5a471f75856b96cce15c2eab1c71a8cb2de7e4cf
narutopig <redgerxu@gmail.com>
2020-11-03 17:54:04 -0800

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Eris from "eris";
-import fs from "fs";
-require("dotenv").config();
+import * as dotenv from "dotenv";
+dotenv.config();
+
 const client = new Eris.CommandClient(process.env.token! ,{},{
     prefix: process.env.prefix!
 });
@@ -12,6 +13,10 @@ client.registerCommand("ping", (message: Eris.Message):void => {
 client.registerCommand("ban", (message: Eris.Message, args: string[]): void => {
     if (!message.member!.permission.json.banMembers) return;
     const target = message.mentions[0];
+    if (target.id == message.author.id) {
+        message.channel.createMessage("You can\'t ban yourself!");
+        return;
+    }
     let reason: string | undefined = args.join(" ");
     if (!reason.replace(/\s/g, '').length) reason = undefined;
     client.banGuildMember(message.guildID!, target.id, 0, reason)
@@ -25,6 +30,10 @@ client.registerCommand("ban", (message: Eris.Message, args: string[]): void => {
 client.registerCommand("kick", (message: Eris.Message, args: string[]): void => {
     if (!(message.member!.permission.json.kickMembers)) return;
     const target = message.mentions[0];
+    if (target.id == message.author.id) {
+        message.channel.createMessage("You can\'t kick yourself!");
+        return;
+    }
     let reason: string | undefined = args.join(" ");
     if (!reason.replace(/\s/g, '').length) reason = undefined;
     client.kickGuildMember(message.guildID!, target.id, reason)
@@ -38,6 +47,10 @@ client.registerCommand("kick", (message: Eris.Message, args: string[]): void => 
 client.registerCommand("mute", (message: Eris.Message, args: string[]): void => {
     if (!(message.member!.permission.json.manageMessages)) return;
     const target = message.mentions[0];
+    if (target.id == message.author.id) {
+        message.channel.createMessage("You can\'t mute yourself!");
+        return;
+    }
     let reason: string | undefined = args.join(" ");
     if (!reason.replace(/\s/g, '').length) reason = undefined;
     const role = message.member!.guild.roles.filter(r => r.name.toLowerCase().includes("mute"))[0];
@@ -54,8 +67,28 @@ client.registerCommand("mute", (message: Eris.Message, args: string[]): void => 
         });
 })
 
+client.registerCommand("unmute", (message: Eris.Message, args: string[]): void => {
+    if (!(message.member!.permission.json.manageMessages)) return;
+    const target = message.mentions[0];
+    let reason: string | undefined = args.join(" ");
+    if (!reason.replace(/\s/g, '').length) reason = undefined;
+    const role = message.member!.guild.roles.filter(r => r.name.toLowerCase().includes("mute"))[0];
+    const roles = message.member!.roles;
+    if (!roles.includes(role.id)) {
+        message.channel.createMessage(`${target.username} is not muted`);
+        return;
+    }
+    client.removeGuildMemberRole(message.guildID!, target.id, role!.id, reason)
+        .then(() => message.channel.createMessage(`Unuted ${target.username}`))
+        .catch((err) => {
+            message.channel.createMessage("An error occured. :(");
+            console.error(err);
+        });
+})
+
 client.on("ready", () => {
     console.log(`${client.user.username} started`);
+    client.editStatus("online", {name: "over Chatlab", type: 3});
 })
 
 client.connect();
