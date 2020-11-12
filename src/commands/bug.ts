f83f076226bb818bc29e6c6b82cca38b1d464ecc
piggo7801 <49803128+bigpig-was-taken@users.noreply.github.com>
2020-11-12 10:24:10 -0800

    Update bug.ts
    made it sound a bit more welcoming

--- src/commands/bug.ts
+++ src/commands/bug.ts
@@ -6,6 +6,6 @@ module.exports = {
     description: "Sends the issues page of the repository",
     usage: "-bug",
     exec: (message: Message): void => {
-        message.channel.send("Have a bug? Visit the issues page of the repo here\nhttps://github.com/narutopig/chatlab-bot/issues");
+        message.channel.send("Have a bug or a suggestion? Visit the issues page of the repo here\nhttps://github.com/narutopig/chatlab-bot/issues");
     }
 };
